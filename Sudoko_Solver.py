739dc772eab6b2a2df45732c0421803e8ecab7ad
Umair Zia <143083285+umairzia1773@users.noreply.github.com>
2024-03-15 22:49:15 +0500

    Add files via upload

---
+++ Sudoko_Solver.py
@@ -0,0 +1,67 @@
+def is_valid(board, row, col, num):
+    # Check if the number is already in the current row
+    if num in board[row]:
+        return False
+
+    # Check if the number is already in the current column
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    # Check if the number is already in the current 3x3 subgrid
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    empty_cell = find_empty_cell(board)
+    if not empty_cell:
+        return True  # If all cells are filled, the puzzle is solved
+
+    row, col = empty_cell
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0  # Backtrack if the current choice doesn't lead to a solution
+
+    return False
+
+def find_empty_cell(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def print_board(board):
+    for row in board:
+        print(" ".join(map(str, row)))
+
+# Example puzzle
+puzzle = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print("Original Puzzle:")
+print_board(puzzle)
+print("\nSolved Puzzle:")
+if solve_sudoku(puzzle):
+    print_board(puzzle)
+else:
+    print("No solution exists.")
